4671ac3b735041b346fc495ce2518ab5a1a39c65
Florian Haas <xahteiwi@gmail.com>
2019-02-19 20:51:20 +0100

    Add encoding comment and unicode_literals

--- presentationhelper/reveal.py
+++ presentationhelper/reveal.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from .generic import PresentationCreator as GenericPresentationCreator
 
 from .generic import IndexRenderer as GenericIndexRenderer
--- tests/test_reveal.py
+++ tests/test_reveal.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 try:
     from io import StringIO
 except ImportError:
