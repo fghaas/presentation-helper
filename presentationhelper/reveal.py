594b4aafa15d8978208c1fa49974e1af32de0779
Florian Haas <xahteiwi@gmail.com>
2019-02-22 21:52:33 +0100

    Simplify package loading logic in RevealTemplateRenderer

--- presentationhelper/reveal.py
+++ presentationhelper/reveal.py
@@ -3,7 +3,7 @@ from .generic import PresentationCreator as GenericPresentationCreator
 
 from .generic import TemplateRenderer as GenericTemplateRenderer
 
-from jinja2 import FileSystemLoader, PackageLoader
+from jinja2 import PackageLoader
 
 import os
 
@@ -32,19 +32,8 @@ class RevealTemplateRenderer(GenericTemplateRenderer):
 
     def _setup_loaders(self):
         super(RevealTemplateRenderer, self)._setup_loaders()
-        loader = None
-        if __file__ == os.path.abspath(__file__):
-            # We're on Python 3.4 and later. __file__ is an absolute
-            # path, we can use PackageLoader.
-            loader = PackageLoader('presentationhelper',
-                                   os.path.join('templates', 'reveal'))
-        else:
-            # __file__ is a relative path, PackageLoader won't
-            # work. Use a FileSystemLoader instead.
-            module_path = os.path.dirname(os.path.abspath(__file__))
-            loader = FileSystemLoader(os.path.join(module_path,
-                                                   'templates',
-                                                   'reveal'))
+        loader = PackageLoader('presentationhelper',
+                               os.path.join('templates', 'reveal'))
         self.loaders.append(loader)
 
 
