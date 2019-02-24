2e8e389b64c2ee1d7e0fde7e87188754aaed47c6
Florian Haas <xahteiwi@gmail.com>
2019-02-24 16:55:56 +0100

    Add Python 3.7 to test matrix
    Include Travis configuration changes as outlined in:
    https://github.com/travis-ci/travis-ci/issues/9069#issuecomment-425720905

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ with open(README) as readme:
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Multimedia :: Graphics :: Presentation',
             'Topic :: Text Processing :: Markup',
         ],
